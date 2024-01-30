0a810844e329fc5ee90d93416c170e91a1f43489
cksk-jp <carlson.ksk@gmail.com>
2024-01-30 17:56:08 -0500

    testing

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 """Blogly application."""
 
 from flask import Flask, flash, redirect, render_template, session
-from sqlalchemy import text
 
-from models import connect_db, db
+from models import Pet, connect_db, db
+
+# from sqlalchemy import text
+
 
 app = Flask(__name__)
+
 app.secret_key = "123-456-789"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pet_shop_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
--- models.py
+++ models.py
@@ -6,7 +6,18 @@ db = SQLAlchemy()
 
 
 def connect_db(app):
-    with app.app_context():
-        db.app = app
-        db.init_app(app)
-        db.create_all()
+    db.app = app
+    db.init_app(app)
+
+
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    def __repr__(self):
+        p = self
+        return f"<Pet id={p.id} name={p.species} hunger={p.hunger}"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    species = db.Column(db.String(30), nullable=True)
+    hunger = db.Column(db.Integer, nullable=False, default=20)
