7adb920f2cf4e0af939e6883cf79be0da12e1832
cksk-jp <carlson.ksk@gmail.com>
2024-02-06 15:05:17 -0500

    Posts refactor progress

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, flash, redirect, render_template, request, url_for
 
-from models import User, connect_db, db, default_img
+from models import Posts, User, connect_db, db, default_img
 
 app = Flask(__name__)
 
@@ -47,7 +47,10 @@ def create_user():
 @app.route("/users/<int:user_id>", methods=["GET"])
 def user_details(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template("user_details.html", title="User details", user=user)
+    posts = user.posts
+    return render_template(
+        "user_details.html", title="User details", user=user, posts=posts
+    )
 
 
 @app.route("/users/<int:user_id>/edit", methods=["GET"])
@@ -90,6 +93,50 @@ def delete_user(user_id):
     else:
         flash("User not found or already deleted", category="error")
     return redirect(url_for("home_page"))
+
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    post = Posts.query.get(post_id)
+    user = post.user
+    return render_template("post.html", title="User Post", user=user, post=post)
+
+
+@app.route("/users/<int:user_id>/posts/new", methods=["GET", "POST"])
+def submit_post(user_id):
+    user = User.query.get(user_id)
+
+    if request.method == "POST":
+        title = request.form.get("post-title")
+        content = request.form.get("post-content")
+
+        new_post = Posts(title=title, content=content, user_id=user_id)
+        db.session.add(new_post)
+        db.session.commit()
+
+        posts = Posts.query.order_by(Posts.created_at).all()
+        return redirect(url_for("user_details", user_id=user_id, posts=posts))
+    else:
+        return render_template("post_form.html", title="New Post", user=user)
+
+
+@app.route("/posts/<int:post_id>/delete", methods=["POST"])
+def delete_post(post_id):
+
+    post = Posts.query.get(post_id)
+
+    if post:
+        user_id = post.user_id
+
+        db.session.delete(post)
+        db.session.commit()
+        flash(
+            f"User {post.title} deleted successfully!",
+            category="success",
+        )
+    else:
+        flash("Post not found or already deleted", category="error")
+    return redirect(url_for("user_details", user_id=user_id))
 
 
 if __name__ == "__main__":
